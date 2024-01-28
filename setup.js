12f7ad76fbf647e39d781243a11fecc0be6b401f
Turd Boomerang <christopherernestarmstrong@gmail.com>
2024-01-28 15:02:02 -0500

    more progress

--- setup.js
+++ setup.js
@@ -1,5 +1,9 @@
 //cd c:/users/chris/documents/html/samfuel
 
+var doneProcessing = false;
+var regularValue = 0;
+var dieselValue = 0;
+
 function setup()
 {
 	const Http = new XMLHttpRequest();
@@ -23,22 +27,55 @@ function processResult(responseText)
 		var regularGas = eastCoast.filter(d => d['product-name'] == "Regular Gasoline");
 		var diesel = eastCoast.filter(d => d['product-name'] == "No 2 Diesel");
 		
-		var regularGasValue = Number.parseFloat(regularGas[0]['value']);
-		var dieselValue = Number.parseFloat(diesel[0]['value']);
+		regularValue = Number.parseFloat(regularGas[0]['value']);
+		dieselValue = Number.parseFloat(diesel[0]['value']);
 		
-		loadContent(regularGasValue, dieselValue);
+		if (!doneProcessing) { loadContent(); }
+		
 	}
 }
 
-function loadContent(regularValue, dieselValue)
+function loadContent()
 {
+	let USDollar = new Intl.NumberFormat('en-US', {
+		style: 'currency',
+		currency: 'USD',
+	});
+	
 	$('#loadingP').css('display', 'none');
 	$('#dataDiv').css('display', 'block');
-	$('#regularPrice').html(`Regular Gas: ${regularValue}`);
-	$('#dieselPrice').html(`Diesel: ${dieselValue}`);
+	$('#regularPrice').html(`Average Price of Regular Gas in New England last month: ${USDollar.format(regularValue)}`);
+	$('#dieselPrice').html(`Average Price of Diesel in New England last month: ${USDollar.format(dieselValue)}`);
 
+	loadTable();
+	
+	doneProcessing = true;
+	
+}
+
+function loadTable()
+{
+	let USDollar = new Intl.NumberFormat('en-US', {
+		style: 'currency',
+		currency: 'USD',
+	});
+	
 	var mpw = Number.parseInt($('#mpwInput')[0].value);
 	var mpy = mpw*52;
+	
+	$('#vehicleTable').html(`<thead><tr><th>Vehicle</th><th>AverageMPG</th><th>MilesperWeek,Year</th><th>GasType</th><th>GasPrice</th><th>YearlyCost</th></tr></thead><tbody></tbody>`);
+	
+	/*<p>New Vehicle Name: <input id='nv_nameInput'></p>
+	<p>New Vehicle MPG: <input id='nv_mpgInput' ></p>
+	<p>New Vehicle Fuel Type:
+		<select id='nv_fuelTypeSelect'>
+			<option value="0">Regular</option>
+			<option value="1">Diesel</option>
+		</select>
+	</p>*/
+	
+	var newVehicle_hasName = $('#nv_nameInput')[0].value != "";
+	var newVehicle_hasValid
 	
 	for (var i = 0; i < vehicles.length; i += 1)
 	{
@@ -54,14 +91,14 @@ function loadContent(regularValue, dieselValue)
 		var yearlyGallons = mpy / vehicles[i].mpg;
 		var yearlyCost = yearlyGallons * gasPrice;
 		
-		
+
 		$('#vehicleTable > tbody:last-child').append(`<tr>
 			<td>${vehicles[i].name}</td>
 			<td>${vehicles[i].mpg}</td>
 			<td>${mpw}, ${mpy}</td>
 			<td>${gasTypeName}</td>
-			<td>${gasPrice}</td>
-			<td>${yearlyCost}</td>
+			<td>${USDollar.format(gasPrice)}</td>
+			<td>${USDollar.format(yearlyCost)}</td>
 		</tr>`);	
 	}
 	
